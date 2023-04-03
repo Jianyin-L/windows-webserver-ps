fa965a6a5ae8f95d804982c83dfb9d0e5042e350
Jenny L <75957599+Jianyin-L@users.noreply.github.com>
2023-04-03 17:07:39 +1000

    Update windows-webserver.ps1

--- windows-webserver.ps1
+++ windows-webserver.ps1
@@ -1,7 +1,7 @@
 # define variables
-$version = "nginx-1.22.1.zip"
-$uri = "http://nginx.org/download/" + $version                                               
-$path = "C:\"
+$version = 'nginx-1.22.1.zip'
+$uri = 'http://nginx.org/download/' + $version                                               
+$path = 'C:\'
 $myname = $env:COMPUTERNAME
 
 # Install Nginx
@@ -9,8 +9,8 @@ Start-BitsTransfer -Source $uri -Destination $path
 Expand-Archive -Path Join-Path($path, $version) -DestinationPath $path
 
 # Set the home page. 
-echo "<html><body><h2>Welcome to Azure! My name is $myname .</h2></body></html>" | Set-Content -Path "C:\1\nginx-1.22.1\html\index.html"
+echo '<html><body><h2>Welcome to Azure! My name is $myname .</h2></body></html>' | Set-Content -Path 'C:\1\nginx-1.22.1\html\index.html'
 
 # $content = Get-Content -Path 'C:\nginx-1.22.1\html\index.html'
 # $newContent = $content -replace 'nginx', $env:computername
-# $newContent | Set-Content -Path "C:\nginx-1.22.1\html\index.html"
+# $newContent | Set-Content -Path 'C:\nginx-1.22.1\html\index.html'
